604e20c7bc086c7cae4f5183505f2bacbea04f51
Eddie Lopez <iameddielopez@gmail.com>
2020-07-11 01:33:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,10 @@ end
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
+
 # in order to complete the rest of this course and be ready for the next course!
 
 
